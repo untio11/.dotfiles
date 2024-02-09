0ee677bb7ca66e63b13075f60f8c3851d2de2113
Robin Kneepkens <robin@skunk.team>
2024-02-09 23:08:47 +0100

    Cleaning up

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -1,35 +1,37 @@
 {
-  description = "Home Manager configuration of robin.kneepkens";
+	description = "Home Manager configuration of robin.kneepkens";
 
-  inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-	nix-colors.url = "github:misterio77/nix-colors";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-	plugin-monokai-pro.url = "github:loctvl842/monokai-pro.nvim";
-	plugin-monokai-pro.flake = false;
-  };
-
-  outputs = { nixpkgs, home-manager, ... }@inputs:
-    let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations."robin.kneepkens" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-		extraSpecialArgs = {
-		  inherit inputs;
+	inputs = {
+		# Specify the source of Home Manager and Nixpkgs.
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		nix-colors.url = "github:misterio77/nix-colors";
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
 		};
-      };
-    };
+		plugin-monokai-pro = {
+			url = "github:loctvl842/monokai-pro.nvim";
+			flake = false;
+		};
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = { nixpkgs, home-manager, flake-utils, ... }@inputs:
+	let
+		pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+	in {
+		homeConfigurations."robin.kneepkens" = home-manager.lib.homeManagerConfiguration {
+			inherit pkgs;
+
+			# Specify your home configuration modules here, for example,
+			# the path to your home.nix.
+			modules = [ ./home.nix ];
+
+			# Optionally use extraSpecialArgs
+			# to pass through arguments to home.nix
+			extraSpecialArgs = {
+				inherit inputs;
+			};
+		};
+	};
 }
--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -15,7 +15,7 @@
     ./features/git.nix
     ./features/alacritty.nix
     ./features/tmux.nix
-	./features/nvim/nvim.nix
+    ./features/nvim/nvim.nix
 	# ./features/zsh/zsh.nix # Currently unstable, don't want to worry about that now.
   ];
 

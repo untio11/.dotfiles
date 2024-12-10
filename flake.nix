757c32940a6c91385b8c40834ed288421f05d75f
Robin Kneepkens <robin.kneepkens@hotmail.com>
2024-12-10 16:57:18 +0000

    Added nixos-wsl config as flake output.

--- flake.nix
+++ flake.nix
@@ -1,17 +1,21 @@
 {
-  description = "My Home Manager configurations for home and work.";
+  description = "Configurations for my various machines.";
 
   inputs = {
-    # The Nix package repository. It contains the derivations of the packages
-    # that we install with home manager.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-colors = {
-      url = "github:misterio77/nix-colors";
+    # For home desktop wsl system.
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-colors = {
+      url = "github:misterio77/nix-colors";
+    };
+    # For animated spinner in hswitch command.
     revolver = {
       url = "github:molovo/revolver";
       flake = false;
@@ -26,6 +30,7 @@
     nixos-wsl = import ./profiles/nixos-wsl.nix {inherit nixpkgs;};
     macos-skunk = import ./profiles/work.nix {inherit nixpkgs;};
   in {
+    # Work macbook. Home manager only.
     homeConfigurations.${macos-skunk.username} = with macos-skunk;
       home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
@@ -35,6 +40,7 @@
           profile = macos-skunk;
         };
       };
+    # Home desktop wsl. Home manager.
     homeConfigurations.${nixos-wsl.username} = with nixos-wsl;
       home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
@@ -43,6 +49,16 @@
           inherit inputs;
           profile = nixos-wsl;
         };
+      };
+    # Home desktop wsl. NixOS config.
+    nixosConfigurations.nixos = with nixos-wsl;
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          {nix.registry.nixpkgs.flake = nixpkgs;}
+          inputs.nixos-wsl.nixosModules.wsl
+          nixos-configuration
+        ];
       };
   };
 }
--- profiles/nixos-wsl.nix
+++ profiles/nixos-wsl.nix
@@ -18,5 +18,48 @@ in rec {
   system = "x86_64-linux";
   pkgs = import nixpkgs {inherit system;};
   username = "untio11";
+  hostName = "pokke-village";
   base-home-dir = "/home";
+  nixos-configuration = {pkgs, ...}: {
+    networking.hostName = hostName;
+    wsl = {
+      enable = true;
+      defaultUser = username;
+      startMenuLaunchers = true;
+    };
+
+    nix.settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      trusted-users = ["root" username];
+    };
+
+    environment.systemPackages = with pkgs; [
+      git
+      helix
+      lsd
+      bat
+    ];
+
+    programs = {
+      nix-ld.enable = true;
+      zsh.enable = true;
+    };
+
+    users.users = {
+      root = {
+        # Otherwise I get an error when logging in as root.
+        extraGroups = ["root"];
+      };
+      # untio11
+      "${username}" = {
+        home = "${base-home-dir}/${username}";
+        shell = pkgs.zsh;
+      };
+    };
+
+    system.stateVersion = "23.11";
+  };
 }

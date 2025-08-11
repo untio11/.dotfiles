5374dc7ea1b730b3dabd2eda8cf8083214f48023
Robin Kneepkens <robin@skunk.team>
2025-08-11 13:38:09 +0200

    Remove direnv personal nix flake workaround USR repo
    Also tweak some other direnv config.

--- profiles/work.nix
+++ profiles/work.nix
@@ -29,9 +29,6 @@ let
         profileExtra = "nxtm() { nx run-many -t test -p \"$1\" --parallel=1 --skip-nx-cache; };";
       };
       helix.settings.theme = "carbonfox";
-      direnv.stdlib = ''
-        source_up_if_exists .envrc.usr-flake
-      '';
     };
     home.packages = with pkgs; [
       jq
--- programs/direnv.nix
+++ programs/direnv.nix
@@ -1,9 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
     enableZshIntegration = true;
-    config.hide_env_diff = true;
+    config = {
+      hide_env_diff = true;
+      warn_timeout = "10s"; # Default is 5s, but that basically always triggers on flakes.
+      strict_env = true; # Runs .envrc with set -euo pipefail
+      bash_path = "${pkgs.bash}/bin/bash"; # Force direnv to always use bash from nix store.
+    };
   };
 }
